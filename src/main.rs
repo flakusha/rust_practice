33719b95ae86cf35e9e242d1c6ba9b1206f08a95
flakusha <zenflak@gmail.com>
2020-04-22 13:14:39 +0300

    Stabilized palindrome

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 #[allow(unused_imports)]
 #[allow(dead_code)]
 
-//mod sum;
-//mod sum_array;
-//mod sum_array_target;
+// mod sum;
+// mod sum_array;
+// mod sum_array_target;
 mod reverse_integer;
 mod integer_max;
+mod palindrome;
 
-//use sum::*;
-//use sum_array::*;
-//use sum_array_target::*;
-use reverse_integer::*;
-use integer_max::*;
+// use sum::*;
+// use sum_array::*;
+// use sum_array_target::*;
+// use reverse_integer::*;
+use integer_max::print_integer_max_values;
+use palindrome::*;
 
 fn main() {
     // println!("Hello, world!");
     // input_sum();
     // input_sum_array();
     // sum_pair_to_target_io();
-    reverse_input_i32(-155000);
+    // reverse_input_i32(-155000);
     print_integer_max_values();
+    println!("{}", check_palindrome(121));
 }
---
+++ src/palindrome.rs
@@ -0,0 +1,60 @@
+// Reverse:
+// Take number.len() - 1 and sum with number.len() - 2
+// Palindrome:
+// true if not negative and num_orig = num_rev
+
+pub fn check_palindrome(main_input: i32) -> bool {
+
+struct Solution {x:i32}
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        const RAD: i32 = 10;
+        let mut input: i32 = x;
+        let original = x;
+
+        if input < 0 {false}
+
+        else {
+
+            let mut rev: i32 = 0;
+            // Reversed version of input
+            while input != 0 {
+                rev = rev * RAD + input % RAD;
+                input /= RAD;
+            }
+            if original == rev {true}
+            else {false}
+        }
+    }
+
+    pub fn is_palindrome_self(&self) -> bool {
+        const RAD: i32 = 10;
+        let mut input: i32 = self.x;
+        let original: i32 = self.x;
+
+        if input < 0 {return false}
+
+        else {
+
+            let mut rev: i32 = 0;
+            // Reversed version of input
+            while input != 0 {
+                rev = rev * RAD + input % RAD;
+                input /= RAD;
+            }
+        if original == rev {return true}
+        else {return false}
+        }
+    }
+}
+
+Solution{x: main_input}.is_palindrome_self()
+
+}
+
+#[cfg(test)]
+    #[test]
+    fn test() {
+        assert_eq!(check_palindrome(121), true)
+    }
