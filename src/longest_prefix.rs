ec0647d03514ac466f8a422f0b3340fea3f8e6ba
flakusha <zenflak@gmail.com>
2020-04-26 23:28:40 +0300

    Reoved skipped task, added nest task

---
+++ src/longest_prefix.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let words: Vec<String> = &strs;
+        let mut prefix = "";
+        let mut prefixes: Vec<String> = [];
+
+        for (word_num, word) in words.iter().enumerate() {
+            
+            
+        }
+        // Start with first letter of the first word
+    }
+}
