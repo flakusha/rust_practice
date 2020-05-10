59e96a0ae7862988f73f8efef02482511a81cc39
flakusha <zenflak@gmail.com>
2020-05-10 18:02:18 +0300

    Formatting, removed debug messages

--- src/histogram_largest_rectangle_area.rs
+++ src/histogram_largest_rectangle_area.rs
@@ -13,7 +13,6 @@ struct BarSerie {
 }
 
 impl Solution {
-    
     pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
         if heights.len() == 0 {return 0i32;}
         else if heights.len() == 1 {return heights[0];}
@@ -26,12 +25,9 @@ impl Solution {
 
         loop {
             let bar: i32 = heights[index];
-            // println!("Starting scan for Element #{}: {}", index, bar);
             let index_scan = index;
             let mut ser: i32 = 1;
-            if index_ignore.contains(&index_scan) {
-                // println!("Skipping #{}", index);
-            }
+            if index_ignore.contains(&index_scan) {}
             else {
                 // Steps to the left and to the right from current index
                 let mut index_scan_left = index_scan;
@@ -43,10 +39,8 @@ impl Solution {
                 loop {
                     // Scan to the left from current index
                     loop {
-                        // println!("Scanning to the left");
                         if index_scan_left == 0 {scan_left = true; break;}
                         else if heights[index_scan_left - 1] == bar {
-                            // println!("Left, works!");
                             ser += 1;
                             index_scan_left -= 1;
                             index_ignore.push(index_scan_left);
@@ -62,11 +56,9 @@ impl Solution {
                     }
                     // Scan to the right from the current index
                     loop {
-                        // println!("Scanning to the right");
                         if index_scan_right >= h_len - 1
                         {scan_right = true; break;}
                         else if heights[index_scan_right + 1] == bar {
-                            // println!("Right, works!");
                             ser += 1;
                             index_scan_right += 1;
                             index_ignore.push(index_scan_right);
@@ -88,28 +80,18 @@ impl Solution {
                     else {continue;}
                 }
             }
-
-            // println!("Finished scan for Element #{}: {}", index, bar);
             if index >= h_len - 1 {
-                // println!("Reached the end!");
-                // println!("{:?}", index_ignore);
                 break;
             } 
             else {
-                // println!("{:?}", index_ignore);
-                // println!("Adding!");
                 index += 1;
             }
         }
-        // println!("{:?}", index_ignore);
-        // println!("Bar series: {:?}", bar_series);
         let mut areas: Vec<i32> = vec![];
         for serie in bar_series.iter() {
             areas.push(serie.h * serie.l);
         }
-        // println!("Areas: {:?}", areas);
         areas.sort();
-        // println!("Areas: {:?}", areas);
         let result = areas[areas.len() - 1];
         result
     }
